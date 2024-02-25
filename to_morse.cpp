2017def1cf6b849b7bf7b5d66e1b0978fe0ba128
Bruce Ussenco <bruceussenco@gmail.com>
2024-02-24 21:43:45 -0300

    translate logic added, without all translation

--- to_morse.cpp
+++ to_morse.cpp
@@ -1,15 +1,84 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+// pairs of a letter and its morse translation
+vector<pair<char, string>> pairs = {
+    {' ', " "},
+    {'a', ".-"},
+    {'b', "-..."},
+    {'c', "-.-."},
+    {'d', "-.."},
+};
+
+int pairsCount = pairs.size();
+
+
+
+string toMorse(char c);
 
 int main(int argc, char **argv)
 {
-    std::cout << '\n' << argc << '\n';
+    cout << '\n';
 
-    for (int i = 0; i < argc; i++) {
-        std::cout << argv[i] << '\n';
+    if (argc == 1) {
+        cout << "[ERROR] Pass some text!\n\n";
+        
+        return 1;
+    }
+    if (argc > 2) {
+        cout << "[ERROR] Pass a single argument\n"
+             << "To do that, place your text inside \"\":\n"
+             << "to_morse \"all the text you want to translate into morse code\"\n\n";
+
+        return 1;
     }
 
-    std::cout << '\n';
+
+    char *text = argv[1];
+    string morse = "";
+
+    cout << "Your text:\n" << text << "\n\n";
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (i != 0) morse += ' ';
+
+        char c = text[i];
+        morse += toMorse(c);
+
+        /*switch (c) {
+            case ' ':
+                morse += ' ';    break;
+            case 'a':
+                morse += ".-";   break;
+            case 'b':
+                morse += "-..."; break;
+            case 'c':
+                morse += "-.-."; break;
+            default:
+                morse += '?'; break;
+        }*/
+    }
+
+    cout << "Morse translation:\n";
+    cout << morse << "\n\n";
 
     return 0;
+}
+
+string toMorse(char c)
+{
+    for (int i = 0; i < pairsCount; i++) {
+        pair p = pairs[i];
+
+        if (c == p.first) {
+            return p.second;
+        }
+    }
+
+    return "?";
 }
 
